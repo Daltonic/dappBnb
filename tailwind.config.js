9d6b2fad51b10bf8dd5cb34119fd314bb4c6c0a1
Daltonic <darlingtongospel@gmail.com>
2023-03-18 07:10:12 +0100

    integrates with Covenants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
       display: ['Open Sans', 'sans-serif'],
